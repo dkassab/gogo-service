1ccfe7dceeadf5e2018a84986910087cc01ea592
Dima Kassab <dkassab@dkassab-mac.local>
2017-01-03 11:47:45 -0500

    adding a new service

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+import (
+"os"
+service "github.com/dkassab/mservice/service")
+)
+
+func main(){
+	port := os.Getenv("PORT")
+	if len(port)==0{
+		port:=3000
+	}
+	server :=service.NewServer()
+	server.Run(":"+port)
+
+}
