0fd376f3ab518df4a747ac7611246c6d29813723
Dima Kassab <dkassab@dkassab-mac.local>
2017-01-09 16:21:10 -0500

    updating server to add practice handler

--- service/server.go
+++ service/server.go
@@ -1,36 +1,48 @@
 package service
 
-import(
-    "net/http"
-    "github.com/codegangsta/negroni"
-    "github.com/gorilla/mux"
-    "github.com/unrolled/render"
-    )
+import (
+	//"fmt"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
+	"net/http"
+)
 
 //NewServer() configures and returns a Server.
 
-func NewServer() *negroni.Negroni{
-	formatter := render.New(render.Options{IndentJSON: true, })
-
-	n:= negroni.Classic()
-	mx :=mux.NewRouter()
-
-	initRoutes(mx, formatter)
+func NewServer() *negroni.Negroni {
+	n := negroni.New()
+	mx := mux.NewRouter()
+	initRoutes(mx)
+	n.Use(negroni.NewLogger())
 	n.UseHandler(mx)
 
 	return n
 }
 
-func initRoutes(mx *mux.Router, formatter *render.Render){
-	
-	mx.HandleFunc("/test",testHandler(formatter)).Methods("GET")
+func initRoutes(mx *mux.Router) {
+	formatter := render.New(render.Options{IndentJSON: true})
+	mx.HandleFunc("/test", testHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/practice", practiceHandler(formatter)).Methods("GET")
 }
 
 func testHandler(formatter *render.Render) http.HandlerFunc {
 
-	return func(w http.ResponseWriter, req *http.Request){
+	return func(w http.ResponseWriter, req *http.Request) {
 
 		formatter.JSON(w, http.StatusOK,
-		struct{Test string }{"This is a test"})
+			struct{ Test string }{"This is a test"})
+	}
+}
+
+func practiceHandler(formatter *render.Render) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, req *http.Request) {
+
+		var a [2]string
+		a[0] = "Hello"
+		a[1] = "World"
+		formatter.JSON(w, http.StatusOK, a)
+		//formatter.JSON(w, http.StatusOK, test1.y)
 	}
 }
